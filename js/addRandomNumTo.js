bc5050ee89eaf5c086033894e435f7083622ddb8
mwcz <mwc@clayto.com>
2016-06-14 16:33:37 -0400

    templatize leaderboard

--- js/addRandomNumTo.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = function (x, multiplier) {
-  var random = Math.ceil(Math.random() * multiplier); // return a random number between 1 and 10
-  return x + random;
-};
--- js/main.js
+++ js/main.js
@@ -1,98 +1,66 @@
 'use strict';
 
 //imports
-var scoreGen = require('./scoreGen');
-var convertScores = require('./convertScores');
-var addRandomNumTo = require('./addRandomNumTo');
-var insertionSort = require('./insertionSort');
+var Ractive = require('ractive');
+var ReconnectingWebSocket = require('reconnectingwebsocket');
+var suffixes = require('./nth-suffix');
 
+var ractive = new Ractive({
+    el: document.body,
+    template: document.querySelector('#template-leaderboard').textContent,
+    data: {
+        leaders: [],
+        suffixes: suffixes,
+    },
+});
 
-var transform = ["transform", "msTransform", "webkitTransform", "mozTransform", "oTransform"];
-var team_1_item = document.getElementById('team-1');
-var team_2_item = document.getElementById('team-2');
-var team_3_item = document.getElementById('team-3');
-var team_4_item = document.getElementById('team-4');
-var team_score_1 = document.getElementById('team-score-1');
-var team_score_2 = document.getElementById('team-score-2');
-var team_score_3 = document.getElementById('team-score-3');
-var team_score_4 = document.getElementById('team-score-4');
-var rank_team_1 = document.getElementById('rank-team-1');
-var rank_team_2 = document.getElementById('rank-team-2');
-var rank_team_3 = document.getElementById('rank-team-3');
-var rank_team_4 = document.getElementById('rank-team-4');
-var transformProperty = getSupportedPropertyName(transform);
-var teams = ['team-1', 'team-2', 'team-3', 'team-4'];
-var teamItems = [
-  team_1_item,
-  team_2_item,
-  team_3_item,
-  team_4_item
-];
-var teamScores = [
-  team_score_1,
-  team_score_2,
-  team_score_3,
-  team_score_4
-];
-var teamRanks = [
-  rank_team_1,
-  rank_team_2,
-  rank_team_3,
-  rank_team_4
-];
-var ranks = [
-  '4<span>th</span>',
-  '3<span>rd</span>',
-  '2<span>nd</span>',
-  '1<span>st</span>'
-];
-var scores = [1,1,1,1];
-var racerPositions = [1,1,1,1];
-var sortedScores = [];
+function setLeaders(leaders) {
+    ractive.set('leaders', leaders);
+}
 
-function getSupportedPropertyName(properties) {
-  for (var i = 0; i < properties.length; i++) {
-    if (typeof document.body.style[properties[i]] != "undefined") {
-      return properties[i];
+/*--- websocket init and handlers ---*/
+
+var ws = new ReconnectingWebSocket('ws://localhost:9001/leaderboard');
+ws.onopen = function wsOpen() { console.log('websocket connection open'); };
+ws.onclose = function wsClose() { console.log('websocket connection closed'); };
+ws.onmessage = function wsMessage(msg) {
+    try {
+        var payload = JSON.parse(msg.data);
+        setLeaders(payload);
+    } catch (e) {
+        console.error(e);
     }
-  }
-  return null;
-}
+};
 
-function getScore() {
-  scores = scoreGen(scores);
-  racerPositions = convertScores(scores);
-}
+/*--- set fake data initially ---*/
 
-function getPosition(i) {
-  var rankIndex;
+var fake_leaders = [
+    {
+        "name": "Mercury Slice",
+        "score": 2729,
+        "cheeves": {
+            "streak5": true,
+            "streak10": true,
+            "streak15": true,
+            "streak50": false,
+            "streak100": false,
+            "streak300": false,
+            "snitch": false
+        }
+    },
+    {
+        "name": "Platinum Serrano",
+        "score": 1929,
+        "cheeves": {
+            "streak5": true,
+            "streak10": false,
+            "streak15": false,
+            "streak50": false,
+            "streak100": false,
+            "streak300": false,
+            "snitch": true
+        }
+    },
+];
 
-  sortedScores = scores.slice(0);
-  //sortedScores.sort(function(a, b){return a-b}); //standard JS way to sort
-  insertionSort(sortedScores, 0, sortedScores.length - 1); //my way, just because I can
-  rankIndex = sortedScores.findIndex(function(a){return a>=scores[i]});
-
-  return rankIndex;
-}
-
-function upPoints() {
-  getScore();
-  for (var i=0; i < racerPositions.length; i++) {
-    var y = racerPositions[i] + '%';
-    if (transformProperty) {
-      teamItems[i].style[transformProperty] = 'translate3d(0, ' + y + ', 0)';
-    }
-    teamScores[i].innerHTML = '<strong>'+ scores[i] +'<strong>';
-    var rankindex = getPosition(i);
-    teamRanks[i].innerHTML = ranks[rankindex];
-    if (rankindex === 3) {
-      teamItems[i].classList.add('winner');
-    } else {
-      teamItems[i].classList.remove('winner');
-    }
-  }
-}
-
-var timer = setInterval(upPoints, 500);
-
-setTimeout(function() {clearInterval(timer)}, 30000);
+setLeaders(fake_leaders);
