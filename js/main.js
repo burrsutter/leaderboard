48b5be539c1c2e2ccd982cdd51245e4cbda5f816
mwcz <mwc@clayto.com>
2016-06-20 15:33:49 -0400

    add real leaderboard ws url for test env

--- js/main.js
+++ js/main.js
@@ -34,7 +34,8 @@ function setLeaders(leaders) {
 
 /*--- websocket init and handlers ---*/
 
-var ws = new ReconnectingWebSocket('ws://localhost:9001/leaderboard');
+// var ws = new ReconnectingWebSocket('ws://localhost:9001/leaderboard');
+var ws = new ReconnectingWebSocket('ws://gamebus-production.apps-test.redhatkeynote.com/leaderboard');
 ws.onopen = function wsOpen() { console.log('websocket connection open'); };
 ws.onclose = function wsClose() { console.log('websocket connection closed'); };
 ws.onmessage = function wsMessage(msg) {
